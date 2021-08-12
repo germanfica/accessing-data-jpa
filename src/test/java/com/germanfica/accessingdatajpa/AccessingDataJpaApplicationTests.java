677ed91aadb3fe32597cf0f435935ec4dcadc2df
German Fica <germafica@gmail.com>
2021-08-12 19:32:22 -0300

    Add maven project
    A maven project with the required JPA and H2 dependencies

---
+++ src/test/java/com/germanfica/accessingdatajpa/AccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germanfica.accessingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
