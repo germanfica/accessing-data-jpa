128e3f86c76784fc10e8d136fbeb46080b2b2b20
German Fica <germafica@gmail.com>
2021-08-12 20:52:12 -0300

    Add TestCustomer class

---
+++ src/main/java/com/germanfica/accessingdatajpa/TestCustomer.java
@@ -0,0 +1,47 @@
+package com.germanfica.accessingdatajpa;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class TestCustomer {
+    private CustomerRepository repository;
+
+    @Autowired
+    public TestCustomer(CustomerRepository repository) {
+        // save a few customers
+        repository.save(new Customer("Jack", "Bauer"));
+        repository.save(new Customer("Chloe", "O'Brian"));
+        repository.save(new Customer("Kim", "Bauer"));
+        repository.save(new Customer("David", "Palmer"));
+        repository.save(new Customer("Michelle", "Dessler"));
+
+        // fetch all customers
+        log.info("Customers found with findAll():");
+        log.info("-------------------------------");
+        for (Customer customer : repository.findAll()) {
+            log.info(customer.toString());
+        }
+        log.info("");
+
+        // fetch an individual customer by ID
+        Customer customer = repository.findById(1L);
+        log.info("Customer found with findById(1L):");
+        log.info("--------------------------------");
+        log.info(customer.toString());
+        log.info("");
+
+        // fetch customers by last name
+        log.info("Customer found with findByLastName('Bauer'):");
+        log.info("--------------------------------------------");
+        repository.findByLastName("Bauer").forEach(bauer -> {
+            log.info(bauer.toString());
+        });
+        // for (Customer bauer : repository.findByLastName("Bauer")) {
+        //  log.info(bauer.toString());
+        // }
+        log.info("");
+    }
+}
