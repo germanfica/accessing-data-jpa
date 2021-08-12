db3e2c4182cb42df442a17f3acf1c19732fa1071
German Fica <germafica@gmail.com>
2021-08-12 19:47:53 -0300

    Create a Simple Entity

---
+++ src/main/java/com/germanfica/accessingdatajpa/Customer.java
@@ -0,0 +1,24 @@
+package com.germanfica.accessingdatajpa;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NonNull
+    private String firstName;
+    @NonNull
+    private String lastName;
+
+    protected Customer() {}
+}
